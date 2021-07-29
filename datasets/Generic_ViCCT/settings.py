db2e6b98846053051155b5ee38271b705a1f5f19
Wightslayer <552188@student.inholland.nl>
2021-07-29 15:30:48 +0200

    More comments

--- datasets/Generic_ViCCT/settings.py
+++ datasets/Generic_ViCCT/settings.py
@@ -2,16 +2,16 @@ from easydict import EasyDict as edict
 
 cfg_data = edict()
 
-cfg_data.TRAIN_BS = 10
-cfg_data.VAL_BS = 1
-cfg_data.N_WORKERS = 4
+cfg_data.TRAIN_BS = 10  # How many crops per training step. Each crop is taken from a separate image.
+cfg_data.VAL_BS = 1  # Must be 1
+cfg_data.N_WORKERS = 4  # Number of workers for the PyTorch dataloader
 
-cfg_data.MEAN_STD = ([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-cfg_data.LABEL_FACTOR = 3000
+cfg_data.MEAN_STD = ([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])  # From ImageNet
+cfg_data.LABEL_FACTOR = 3000  # Scale each pixel in the GT density maps by this value
 
-cfg_data.OVERLAP = 8  # For test images, how much overlap should crops have
-cfg_data.IGNORE_BUFFER = 4  # When reconstructing the complete density map, how many pixels of edges should be ignored
-                             # No pixels are ignored at the edges of the density map.
+cfg_data.OVERLAP = 8        # For test images, how much overlap should crops have
+cfg_data.IGNORE_BUFFER = 4  # When reconstructing the complete density map, how many pixels of edges of the crops
+#                             should be ignored. No pixels are ignored at the edges of the complete density map.
 
 
 # =========================================================================== #
